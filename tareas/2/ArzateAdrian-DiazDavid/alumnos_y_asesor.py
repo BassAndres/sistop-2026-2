3a0ccbf98c5ad89462f23dec94a6221421a73f34
David <david@pc-bd-dda.fi.unam>
2026-03-26 23:14:23 -0600

    Se terminó con la implementación del problema

--- tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
+++ tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
@@ -9,6 +9,7 @@ numero_sillas = 10
 
 
 class Alumno(th.Thread):
+	# Constructor de la clase Alumno
 	def __init__(self, id, sillas, turno, condicion):
 		super().__init__()
 		self.id = id
@@ -17,6 +18,7 @@ class Alumno(th.Thread):
 		self.condicion = condicion
 		self.numero_dudas = random.randint(1, 10)
 
+	# comportamiento del hilo Alumno
 	def run(self):
 		with self.silla:
 
@@ -26,12 +28,13 @@ class Alumno(th.Thread):
 
 		print(f"El alumno {self.id} salió del cubiculo")
 
+	# Resolucion de dudas
 	def resolver_dudas(self):
 		while self.numero_dudas != 0:
 			
 			with self.turno:
 				self.condicion.notify()
-				print(f"El alumno {self.id} está relaizando resolviendo una duda")
+				print(f"El alumno {self.id} está resolviendo una duda")
 				self.condicion.wait()
 				self.numero_dudas -= 1
 							
@@ -47,13 +50,13 @@ class Profesor(th.Thread):
 	def run(self):
 		with self.condicion:
 			while True:
+				self.condicion.wait()
 				print(f"El Profesor está resolviendo una duda")
 				
 				time.sleep(1)
 
 				self.condicion.notify()
-				print(f"El Profesor terminó con una duda")
-				self.condicion.wait()			
+				print(f"El Profesor terminó con una duda")		
 
 def main():
 	
@@ -75,6 +78,7 @@ def main():
 			alumno = Alumno(id_alumno, numero_sillas, turno, condicion)
 			alumno.start()
 			id_alumno += 1
+			time.sleep(3)
 	except KeyboardInterrupt:
 		print("finalizacion")
 
