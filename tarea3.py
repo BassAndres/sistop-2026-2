2623922ca619f42f13dd630abf3af0362cd8ba67
Andres Basilio <andresbasilio041@gmail.com>
2026-04-16 14:18:36 -0600

    Round robin hecho con clases

--- tarea3.py
+++ tarea3.py
@@ -1,22 +1,31 @@
-procesos = [{'id': 'A', 'llegada': 2, 'rafaga': 3}, {'id': 'B', 'llegada': 4, 'rafaga': 2}]
+class Proceso:
+    def __init__(self, n, l, t):
+        self.n, self.l, self.r = n, l, t
+
+procesos = [Proceso('A', 0, 3), Proceso('B', 1, 2)]
 t = 0
 listos = []
 ejecutando = None
+quantum = 2
+q_restante = 0
+
 while procesos or listos or ejecutando:
-    # Llegan procesos
     for p in procesos[:]:
-        if p['llegada'] == t:
+        if p.l == t:
             listos.append(p)
             procesos.remove(p)
             
     if ejecutando is None and listos:
         ejecutando = listos.pop(0)
+        q_restante = quantum 
         
-    if ejecutando is None:
-        print(f"t={t}: [Ocioso]")
-    else:
-        print(f"t={t}: Ejecuta {ejecutando['id']}")
-        ejecutando['rafaga'] -= 1
-        if ejecutando['rafaga'] == 0:
+    if ejecutando:
+        print(f"t={t}: {ejecutando.n}")
+        ejecutando.r -= 1
+        q_restante -= 1
+        if ejecutando.r == 0:
+            ejecutando = None
+        elif q_restante == 0:
+            listos.append(ejecutando)
             ejecutando = None
     t += 1
