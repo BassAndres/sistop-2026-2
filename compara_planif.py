2ff815bd23b90c27f04a1d8b259a7f731b84103c
Andres Basilio <andresbasilio041@gmail.com>
2026-04-16 14:10:03 -0600

    Simulación por ticks

--- compara_planif.py
+++ compara_planif.py
@@ -1,10 +1,17 @@
-procesos = [('A', 0, 3), ('B', 1, 2), ('C', 2, 1)]
-tiempo_actual = 0
+procesos = [('A', 2, 3), ('B', 4, 2)] # Nota: Nadie llega n t=0
+t = 0
+listos = []
 
-print("Simulación FCFS Básica:")
-for nombre, llegada, total in procesos:
-    if tiempo_actual < llegada:
-        tiempo_actual = llegada  # Avanza el tiempo si hay espera
-    print(f"t={tiempo_actual}: Inicia {nombre}")
-    tiempo_actual += total
-    print(f"t={tiempo_actual}: Termina {nombre}")
+while procesos or listos:
+    
+    for p in procesos[:]:
+        if p[1] == t:
+            listos.append(p)
+            procesos.remove(p)
+    
+    if listos:
+        actual = listos[0]
+        print(f"t={t}: Ejecutando {actual[0]}")
+        
+
+        break 
